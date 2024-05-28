33ae6382dda10d9a65ad03929e37f272d49cc0f7
Gerardo Pérez <yerko.pg@gmail.com>
2024-05-28 21:14:23 +0200

    Update basic_exercises.js

--- basic_exercises.js
+++ basic_exercises.js
@@ -11,3 +11,14 @@ let johnBMI = johnWeight / (johnHeight ** 2);
 let markHigherBMI = markBMI > johnBMI;
 
 console.log('Mark BMI is ' + markBMI + ' and John BMI is ' + johnBMI + ' ' + markHigherBMI);
+
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas){
+    console.log("Dolphins win the trophy");
+}else if (scoreDolphins < scoreKoalas){
+    console.log("Koalas win the trophy");
+}else if((scoreDolphins === scoreKoalas)){
+    console.log("Both win the trophy");
+}
